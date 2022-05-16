d83716ad483b8a7cc04a49bfaefd58e01fba52d5
Shobhit Maurya <62472111+mauryashobhit@users.noreply.github.com>
2022-05-16 13:59:16 +0530

    Add files via upload

--- games/views.py
+++ games/views.py
@@ -7,16 +7,11 @@ from lxml import etree
 import requests
 import urllib
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 import urllib.request
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .forms import  CreateUserForm
+from .forms import  Login
 from django.contrib import messages
-from sklearn.preprocessing import LabelEncoder
-le=LabelEncoder()
-from sklearn.metrics.pairwise import cosine_similarity
 import wikipedia
 from django.conf import settings
 from isodate import parse_duration
@@ -41,14 +36,10 @@ def user_index(request):
     return render(request,'user_index.html')
 
 def ps_recommend(request):
-    suggestion=get_suggestions_ps()
-    return render(request,'ps_recommend.html',context={"suggestion":suggestion})
+    return render(request,'ps_recommend.html')
 
-def xbox_recommend(request):
-    return render(request,'xbox_recommend.html')
-
-def xbox_suggestions(request):
-    data=pd.read_csv("final_xbox.csv")
+def ps_suggestions(request):
+    data=pd.read_csv("final_ps.csv")
     Director=""
     Publisher=""
     Genre=""
@@ -94,7 +85,66 @@ def xbox_suggestions(request):
     Genre=data.loc[data['Title'] == Title, 'Genre(s)'].iloc[0]
     list=wikipedia.summary(Title, sentences=20)
     df=data.loc[data['Genre(s)']==Genre]
+    df1=data.loc[data['Publisher(s)']==Publisher]
     df=df["Title"].tolist()
+    df1=df1["Title"].tolist()
+    df.remove(Title)
+    df1.remove(Title)
+    return render(request,"ps_recommend.html",context={'df':df,'videos' : videos,"Director":Director,"Publisher":Publisher,"Title":Title,"Genre":Genre,"list":list})
+
+
+def xbox_recommend(request):
+    return render(request,'xbox_recommend.html')
+
+def xbox_suggestions(request):
+    data=pd.read_csv("final_xbox.csv")
+    Director=""
+    Publisher=""
+    Genre=""
+    list=""
+    videos=[]
+    if request.method == 'POST':
+        Title= request.POST.get('Title')
+        search_url = 'https://www.googleapis.com/youtube/v3/search'
+        video_url = 'https://www.googleapis.com/youtube/v3/videos'
+        search_params = {
+            'part' : 'snippet',
+            'q' : request.POST['Title'],
+            'key' : settings.YOUTUBE_DATA_API_KEY,
+            'maxResults' : 9,
+            'type' : 'video'
+        }
+        r = requests.get(search_url, params=search_params)
+        results = r.json()['items']
+        video_ids = []
+        for result in results:
+            video_ids.append(result['id']['videoId'])
+        if request.POST['submit'] == 'lucky':
+            return redirect(f'https://www.youtube.com/watch?v={ video_ids[0] }')
+        video_params = {
+            'key' : settings.YOUTUBE_DATA_API_KEY,
+            'part' : 'snippet,contentDetails',
+            'id' : ','.join(video_ids),
+            'maxResults' : 9
+        }
+        r = requests.get(video_url, params=video_params)
+        results = r.json()['items']
+        for result in results:
+            video_data = {
+                'title' : result['snippet']['title'],
+                'id' : result['id'],
+                'url' : f'https://www.youtube.com/watch?v={ result["id"] }',
+                'duration' : int(parse_duration(result['contentDetails']['duration']).total_seconds() // 60),
+                'thumbnail' : result['snippet']['thumbnails']['high']['url']
+            }
+            videos.append(video_data)
+    Director=data.loc[data['Title'] == Title, 'Developer(s)'].iloc[0]
+    Publisher=data.loc[data['Title'] == Title, 'Publisher(s)'].iloc[0]
+    Genre=data.loc[data['Title'] == Title, 'Genre(s)'].iloc[0]
+    list=wikipedia.summary(Title, sentences=20)
+    df=data.loc[data['Genre(s)']==Genre]
+    df=df["Title"].tolist()
+    df.remove(Title)
     return render(request,"xbox_recommend.html",context={'df':df,'videos' : videos,"Director":Director,"Publisher":Publisher,"Title":Title,"Genre":Genre,"list":list})
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------
